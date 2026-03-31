24c8413aa99342466c5a65bf623faf4a492980bb
Abdulaziz1001001 <azizshaban73@gmail.com>
2026-03-31 16:03:20 +0300

    Delete models directory

--- models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const AdminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  email: String
-});
-module.exports = mongoose.model('Admin', AdminSchema);
--- models/Group.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const GroupSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  desc: String,
-  color: { type: String, default: '#C45A28' }
-});
-module.exports = mongoose.model('Group', GroupSchema);
